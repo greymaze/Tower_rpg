baf7952529e90ea4b45af0342c1dcf6dbd0672ab
greymaze <jeremypcha@outlook.com>
2025-08-14 01:56:27 +0200

    Create caractère_screen.dart

---
+++ lib/screens/caractère_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CharacterScreen extends StatelessWidget {
+ @override
+ Widget build(BuildContext context) {
+   return Scaffold(
+     appBar: AppBar(title: Text('Personnage')),
+     body: Center(child: Text('Création et personnalisation du personnage')),
+   );
+ }
+}
