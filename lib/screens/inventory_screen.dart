b70c16f99fb31299bef26d9515806bfdf3bd8a2b
greymaze <jeremypcha@outlook.com>
2025-08-14 01:57:26 +0200

    Create inventory_screen.dart

---
+++ lib/screens/inventory_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class InventoryScreen extends StatelessWidget {
+ @override
+ Widget build(BuildContext context) {
+   return Scaffold(
+     appBar: AppBar(title: Text('Inventaire')),
+     body: Center(child: Text('Gestion de l’équipement et slots')),
+   );
+ }
+}
