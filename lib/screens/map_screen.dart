128de999de6941a958b989ab3e42139e7f93c554
greymaze <jeremypcha@outlook.com>
2025-08-14 01:55:13 +0200

    Create map_screen.dart

---
+++ lib/screens/map_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MapScreen extends StatelessWidget {
+ @override
+ Widget build(BuildContext context) {
+   return Scaffold(
+     appBar: AppBar(title: Text('Carte de la Tour')),
+     body: Center(child: Text('Exploration par étage et mini-cartes placeholder')),
+   );
+ }
+}
