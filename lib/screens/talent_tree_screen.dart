6a187fff0d4dc79a7cf8d017637e944952318ecb
greymaze <jeremypcha@outlook.com>
2025-08-14 02:07:56 +0200

    Create talent_tree_screen.dart

---
+++ lib/screens/talent_tree_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class TalentTreeScreen extends StatelessWidget {
+ @override
+ Widget build(BuildContext context) {
+   return Scaffold(
+     appBar: AppBar(title: Text('Arbre de talents')),
+     body: Center(child: Text('Sélection de talents avec prérequis')),
+   );
+ }
+}
