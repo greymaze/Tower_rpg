3844c8fa372a587ddb5b3336948b1dcf3af842c5
greymaze <jeremypcha@outlook.com>
2025-08-14 02:12:21 +0200

    Create monster.dart

---
+++ lib/models/monster.dart
@@ -0,0 +1,13 @@
+class Monster {
+ String name;
+ int level;
+ int health;
+ int damage;
+
+ Monster({
+   required this.name,
+   required this.level,
+   required this.health,
+   required this.damage,
+ });
+}
